9b86cbe56a988da762b40c88d676b25ec697a422
Howard Zhang <howardzhang@Howards-MacBook-Pro.local>
2020-12-06 22:16:38 +0800

    music app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+import {useState, useEffect} from 'react';
+import Player from './components/Player';
 
 function App() {
+
+  const [songs] = useState([
+    {
+      title:'Unstoppable',
+      artist:'AJ Mitchell',
+      img_src:'./images/ajmitchell.jpeg',
+      src:'./music/unstoppable.mp3'
+    },
+    {
+      title:'Jingle',
+      artist:'Justin Bieber',
+      img_src:'./images/justinbieber.jpg',
+      src:'./music/jingle.mp3'
+    },
+    {
+      title:'Groove',
+      artist:'Chris Brown',
+      img_src:'./images/chrisbrown.jpg',
+      src:'./music/groove.mp3'
+    }
+  ]);
+
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+  const [NextSongIndex, setNextSongIndex] = useState(currentSongIndex + 1 );
+
+  useEffect(()=>{
+ 
+    setNextSongIndex(()=>{
+      if (currentSongIndex+1>songs.length-1){
+        return 0;
+      }else{
+        return currentSongIndex +1;
+      }
+    });
+
+  },[currentSongIndex]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Player 
+      currentSongIndex={currentSongIndex}
+      setCurrentSongIndex={setCurrentSongIndex}
+      NextSongIndex={NextSongIndex}
+      songs={songs}
+      />
     </div>
   );
 }
